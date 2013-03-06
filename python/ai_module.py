3ac87e7b3ba6882dc00d1efe8d72b40cb5ab0ff6
Mads Dørup <mads@dorup.dk>
2013-03-06 18:41:57 +0100

    Some weird newline stuff

--- python/ai_module.py
+++ python/ai_module.py
@@ -13,34 +13,34 @@ class AI(object):
     
     The locations are transformed so that the player the AI is playing for has backline on row 1.
     """
-    
+
     def __init__(self, name):
 
         ai_type = imp.load_source(name, "ai_" + name.lower() + ".py")
         self.get_action = ai_type.get_action
         self.put_counter = ai_type.put_counter
         self.name = name
-            
+
     def select_action(self, p):
-                 
+
         p = gamestate.copy_p(p)
-                    
+
         if p[0].backline == 8:
             p = get_transformed_p(p)
             transform_action = get_transformed_action
         else:
             transform_action = get_same_action
-        
+
         actions = mover.get_actions(p)
 
         if actions:
             action = self.get_action(p, actions)
-            return transform_action(action)     
+            return transform_action(action)
         else:
-            return None  
+            return None
 
     def add_counters(self, p):
-        
+
         for unit in p[0].units.values():
             if unit.xp == 2:
                 if unit.defence + unit.dcounters == 4:
@@ -57,10 +57,10 @@ class Direction:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        
+
     def move(self, pos):
         return (pos[0] + self. x, pos[1] + self.y)
-    
+
     def perpendicular(self, pos):
         return ((pos[0] + self.y, pos[1] + self.x), (pos[0] - self.y, pos[1] - self.x))
 
@@ -73,10 +73,9 @@ def t(pos):
 
 
 def get_transformed_direction(direction):
-    
+
     if direction.y == -1:
         return Direction(0,1)
- 
     if direction.y == 1:
         return Direction(0,-1)
 
@@ -92,7 +91,7 @@ def get_transformed_action(action):
         sub_action = get_transformed_action(sub_action)
     if hasattr(action, "push"):
         action.push_direction = get_transformed_direction(action.push_direction)
-        
+
     return action
 
 
@@ -104,7 +103,7 @@ def get_same_action(action):
 def get_transformed_p(p):
 
     newp = []
-    
+
     for player in p:
         pc = copy.copy(player)
         pc.newunits = {}
@@ -113,6 +112,5 @@ def get_transformed_p(p):
         pc.units = pc.newunits
         pc.backline =  9 - player.backline
         newp.append(pc)
-        
-    return newp
 
+    return newp
