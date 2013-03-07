5b874b3f0583ea39621950c63307dd6f5ff93fab
Mads Dørup <mads@dorup.dk>
2013-03-07 01:55:19 +0100

    Add test case, parser for test case as well as a single unit test

---
+++ python/tests.py
@@ -0,0 +1,89 @@
+import unittest
+import re
+import units_module
+import ai_module
+from player import Player
+
+
+class TestAI(unittest.TestCase):
+    def test_AIEvaluator_WhenMoveAttackIsPossible_ThenItShouldBeChosen(self):
+        self.parse_test_case()
+
+    def parse_test_case(self):
+        test_file = open('tests.txt', 'r')
+
+        self.assertTrue(re.search('^== [A-Za-z0-9-_]+ ==\r?\n$', test_file.readline()),
+                        'Please begin test specification with "== Test_Name ==')
+
+        player1 = self.parse_player(test_file)
+        player2 = self.parse_player(test_file)
+
+        self.assertTrue(re.search('^Expected outcome:\r?\n$', test_file.readline()),
+                        'Please specify an expected outcome')
+
+        self.assertNotEqual(player1.actions_remaining, player2.actions_remaining, 'It is noones turn')
+
+        game_state = [player1, player2]
+
+        if player2.actions_remaining > player1.actions_remaining:
+            game_state.reverse()
+
+        self.assertNotEqual('Human', game_state[0].ai, 'Active player is a human. It should be a computer')
+
+        match = re.search('(.*)\r?\n', test_file.readline())
+        expected_outcome = match.group(1)
+
+        action = game_state[0].ai.select_action(game_state)
+
+        print 'Expected outcome: ' + expected_outcome
+        print 'Actual outcome: ' + str(action)
+
+        self.assertEquals(expected_outcome, str(action), 'The outcome was not as expected')
+
+    def parse_player(self, test_file):
+        self.assertTrue(re.search('^Player(1|2):\r?\n$', test_file.readline()),
+                        'Incorrect player specification. Please write: Player1/Player2')
+
+        match = re.search('^(Red|Green)\r?\n$', test_file.readline())
+        self.assertTrue(match, 'Incorrect player color specification. Please write: Red/Green')
+        player = Player(match.group(1))
+
+        match = re.search('^([A-Za-z]+)\r?\n', test_file.readline())
+        self.assertTrue(match, 'Incorrect player specification. Please write either Human or a named AI')
+        if match.group(1) == 'Human':
+            player.ai = match.group(1)
+        else:
+            player.ai = ai_module.AI(match.group(1))
+
+        match = re.search('^Actions: ([0-2])\r?\n$', test_file.readline())
+        self.assertTrue(match, 'Incorrect action specification. Please write "Actions: [0..2]"')
+        player.actions_remaining = int(match.group(1))
+
+        match = re.search('^Extra action: (True|False)\r?\n$', test_file.readline())
+        self.assertTrue(match, 'Incorrect extra action specification. Please write "Extra action: True/False"')
+        if match.group(1) == 'True':
+            player.extra_action = True
+
+        self.assertTrue(re.search(test_file.readline(), '\r?\n'), 'Please wrap unit specifications in empty lines')
+
+        units = {}
+        line = test_file.readline()
+        while line != '' and line != '\r\n' and line != '\n':
+            match = re.search('^([A-E][0-8])\r?\n$', line)
+            self.assertTrue(match, 'Incorrect unit specification. Please write something like "A1<newline>Archer')
+
+            position = (ord(match.group(1)[0]) - 64, int(match.group(1)[1]))
+
+            line = test_file.readline()
+            match = re.search('^([A-Za-z -_]+)\r?\n$', line)
+
+            units[position] = getattr(units_module, match.group(1))(player.color)
+
+            self.assertTrue(re.search(test_file.readline(), '^\r?\n$'), 'Please wrap unit specifications in empty lines')
+            line = test_file.readline()
+        player.units = units
+        return player
+
+
+if __name__ == '__main__':
+    unittest.main()
