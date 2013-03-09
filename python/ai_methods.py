650c91efd328a1fdc1b8d03955476d8d29ba5172
Jonatan Pallesen <jonatan.smp@gmail.com>
2013-03-09 01:12:22 -0800

    Rewrite to use gamestate

--- python/ai_methods.py
+++ python/ai_methods.py
@@ -2,33 +2,30 @@ from __future__ import division
 import battle
 from ai_module import get_transformed_action
 import copy
-import gamestate
-import settings
 
 
-
-def document_actions(ai_type, actions, p):
+def document_actions(actions, g):
     
-    if p[0].actions_remaining == 1:
-        taction = "1"
+    if g.players[0].actions_remaining == 1:
+        current_action = "1"
     else:
-        taction = "2"
+        current_action = "2"
         
-    if hasattr(p[0], "extra_action"):
-        taction += ".2"
+    if hasattr(g.players[0], "extra_action"):
+        current_action += ".2"
     
-    out = open("./replay/" + p[0].color + " AI actions " + str(settings.turn) + "." + taction + ".txt", 'w')
+    out = open("./replay/" + g.players[0].color + " AI actions " + str(g.turn) + "." + current_action + ".txt", 'w')
     
     for action in actions:
-        if p[0].color == "Red":
-            #action = copy.deepcopy(action)
-            action = gamestate.copy_action(action)
-            get_transformed_action(action)
+        if g.players[0].color == "Red":
+            action = copy.copy(action)
+            action = get_transformed_action(action)
             
         out.write(str(action) + "\n")
-        out.write("Score: " + str(round(action.score,2)) + "\n")
+        out.write("Score: " + str(round(action.score, 2)) + "\n")
         if hasattr(action, "score_success"):
-            out.write("Score sucess: " + str(round(action.score_success)) + " Score failure: " + str(round(action.score_failure)) + "\n")
+            out.write("Score success: " + str(round(action.score_success)) + " Score failure: " +
+                      str(round(action.score_failure)) + "\n")
         out.write("\n")
     out.close()
 
