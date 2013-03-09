0e5f86fe14b683857bb95a82677fc74b09b7a72b
Jonatan Pallesen <jonatan.smp@gmail.com>
2013-03-09 02:31:16 -0800

    Visualize cannon counters (I feel like I already did this)

--- python/main.py
+++ python/main.py
@@ -146,10 +146,13 @@ def add_ycounters(unit):
     if hasattr(unit, "extra_life"): unit.ycounters = 1
     else: unit.ycounters = 0
 
+
 def add_bcounters(unit):
     unit.bcounters = 0
     if hasattr(unit, "frozen"):
         unit.bcounters = unit.frozen
+    if hasattr(unit, "attack_frozen"):
+        unit.bcounters = unit.attack_frozen
     if hasattr(unit, "just_bribed"):
         unit.bcounters = 1
 
