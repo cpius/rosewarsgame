4268147dae4a54b5e48936cb88e4d9e197b5e3e9
Mads Dorup <mads@dorup.dk>
2013-03-16 22:12:03 +0100

    Bring testspeed up to speed

--- python/testspeed.py
+++ python/testspeed.py
@@ -1,28 +1,28 @@
 from __future__ import division
-import gamestate_module as gamestate
+from gamestate_module import Gamestate
 import settings
 import setup
 from time import time
-import copy
 from player import Player
-import pickle
 import pstats
 import cProfile
+import saver
 
 
 def make_gamestate():
     player1, player2 = Player("Green"), Player("Red")
 
-    player1.ai_name, player2.ai_name = settings.player1_ai, settings.player2_ai
+    player1.ai_name = "Evaluator"
+    player2.ai_name = "Human"
 
     player1_units, player2_units = setup.get_start_units()
 
-    g = gamestate.Gamestate(player1, player1_units, player2, player2_units, 1, 2)
+    gamestate = Gamestate(player1, player1_units, player2, player2_units, 1, 2)
 
-    g.initialize_turn()
-    g.initialize_action()
+    gamestate.initialize_turn()
+    gamestate.initialize_action()
 
-    return g
+    return gamestate
 
 
 def timer(function, *args):
@@ -33,25 +33,25 @@ def timer(function, *args):
     return function.__name__, round((time() - t) * 10, 5)
 
 
-def time_ai(ai_name, g):
-    g.players[0].ai_name = ai_name
-    g.players[1].ai_name = "Human"
-    g.set_ais()
+def time_ai(ai_name, gamestate):
+    gamestate.players[0].ai_name = ai_name
+    gamestate.players[1].ai_name = "Human"
+    gamestate.set_ais()
 
     t = time()
     for i in range(10):
-        g.players[0].ai.select_action(g)
+        gamestate.players[0].ai.select_action(gamestate)
     return "Select action", ai_name, round((time() - t) * 100, 5)
 
 
-def load_gamestates(saved_g):
+def load_gamestates(saved_gamestate):
     for i in range(10000):
-        gamestate.load_gamestate(saved_g)
+        saver.load_gamestate(saved_gamestate)
 
 
 def get_many_actions():
     for i in range(1000):
-        g.get_actions()
+        gamestate.get_actions()
 
 
 def profiling(function_string):
@@ -64,10 +64,9 @@ def profiling(function_string):
     p.print_stats()
 
 
-g = make_gamestate()
+gamestate = make_gamestate()
 
-g.set_ais()
+gamestate.set_ais()
 
 
-profiling("g.players[0].ai.select_action(g)")
-
+profiling("gamestate.current_player().ai.select_action(gamestate)")
