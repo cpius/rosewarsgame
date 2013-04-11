0bd62a3754cd2dcf3ae5c9fd882d41f6dfc0d3e2
Mads Dorup <mads@dorup.dk>
2013-04-11 02:03:23 +0200

    Introduce new test: WhenAnActionIsTaken_ItShouldBeReflected

--- python/tests.py
+++ python/tests.py
@@ -12,6 +12,26 @@ from pprint import PrettyPrinter
 
 
 class TestAI(unittest.TestCase):
+    def test_GamestateDocument_WhenAnActionIsTaken_ItShouldBeReflected(self):
+        converter = DocumentConverter()
+        gamestate_document = self.get_test_gamestate_document()
+        gamestate = converter.document_to_gamestate(gamestate_document)
+        action = converter.document_to_action({"start_position": "D6", "end_position": "D7"})
+
+        gamestate.do_action(action)
+        gamestate.shift_turn_if_done()
+
+        actual = converter.gamestate_to_document(gamestate)
+        expected = gamestate_document
+
+        player2_units = expected["player2_units"]
+        expected["player2_units"] = {"D7": expected["player1_units"]["D6"]}
+        expected["player1_units"] = player2_units
+        expected["turn"] += 1
+        expected["actions_remaining"] = 2
+
+        self.assert_equal_documents(expected, actual)
+
     def test_GamestateDocument_WhenSavingAndLoadingDocument_ThenItShouldBeTheSame(self):
         document = self.get_test_gamestate_document()
         converter = DocumentConverter()
