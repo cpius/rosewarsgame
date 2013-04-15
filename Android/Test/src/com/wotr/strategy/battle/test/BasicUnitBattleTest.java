a85f536226b5a9eec5b79206770373519a3eaf81
Peer Bech Hansen <peer.bech.hansen@gmail.com>
2013-04-15 17:56:11 +0200

    Added Tests

--- Android/Test/src/com/wotr/strategy/battle/test/BasicUnitBattleTest.java
+++ Android/Test/src/com/wotr/strategy/battle/test/BasicUnitBattleTest.java
@@ -1,30 +1,55 @@
 package com.wotr.strategy.battle.test;
 
+import static org.mockito.Mockito.when;
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.wotr.GameManager;
-import com.wotr.model.unit.Archer;
-import com.wotr.model.unit.HeavyCavalry;
-import com.wotr.model.unit.Pikeman;
 import com.wotr.model.unit.Unit;
-import com.wotr.strategy.DiceStrategy;
+import com.wotr.model.unit.basic.Archer;
+import com.wotr.model.unit.basic.HeavyCavalry;
+import com.wotr.model.unit.basic.Pikeman;
 import com.wotr.strategy.battle.BattleStrategy;
-import com.wotr.strategy.factory.DefaultGameFactory;
+import com.wotr.strategy.battle.DefaultAttackStrategy;
+import com.wotr.strategy.battle.DefaultBattleStrategy;
+import com.wotr.strategy.battle.DefaultBonusStrategy;
+import com.wotr.strategy.battle.DefaultDefenceStrategy;
+import com.wotr.strategy.battle.unit.ArcherAttackStrategy;
+import com.wotr.strategy.battle.unit.PikemanAttackStrategy;
+import com.wotr.strategy.factory.GameFactory;
 import com.wotr.strategy.impl.StaticDiceStrategy;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BasicUnitBattleTest {
+
+	@Mock
+	private GameFactory gameFactory;
+
+	@Before
+	public void setUp() throws Exception {
+
+		when(gameFactory.getBattleStrategy()).thenReturn(new DefaultBattleStrategy());
+
+		when(gameFactory.getPikemanAttackStrategy()).thenReturn(new PikemanAttackStrategy());
+		when(gameFactory.getArcherAttackStrategy()).thenReturn(new ArcherAttackStrategy());
+		when(gameFactory.getAttackStrategy()).thenReturn(new DefaultAttackStrategy());
+		when(gameFactory.getDefenceStrategy()).thenReturn(new DefaultDefenceStrategy());
+		
+		when(gameFactory.getBonusStrategy()).thenReturn(new DefaultBonusStrategy());
+
+	}
 
 	@Test
 	public void testSuccesfulArcherVSArcherAttack() {
 
-		BattleStrategy bs = GameManager.setFactory(new DefaultGameFactory() {
-			@Override
-			public DiceStrategy getDiceStrategy() {
-				return new StaticDiceStrategy(4, 3);
-			}
-		}).getBattleStrategy();
+		BattleStrategy bs = GameManager.setFactory(gameFactory).getBattleStrategy();
+
+		when(gameFactory.getDiceStrategy()).thenReturn(new StaticDiceStrategy(4, 3));
 
 		TestBattleListener tbl = new TestBattleListener();
 
@@ -35,18 +60,14 @@ public class BasicUnitBattleTest {
 		boolean success = bs.battle(attackingUnit, defendingUnit);
 		Assert.assertTrue(success);
 		Assert.assertEquals(101011, tbl.getObservations());
-
 	}
 
 	@Test
 	public void testNonSuccesfulArcherVSArcherAttack() {
 
-		BattleStrategy bs = GameManager.setFactory(new DefaultGameFactory() {
-			@Override
-			public DiceStrategy getDiceStrategy() {
-				return new StaticDiceStrategy(3);
-			}
-		}).getBattleStrategy();
+		BattleStrategy bs = GameManager.setFactory(gameFactory).getBattleStrategy();
+
+		when(gameFactory.getDiceStrategy()).thenReturn(new StaticDiceStrategy(3));
 
 		TestBattleListener tbl = new TestBattleListener();
 
@@ -62,12 +83,9 @@ public class BasicUnitBattleTest {
 	@Test
 	public void testDefendedArcherVSArcherAttack() {
 
-		BattleStrategy bs = GameManager.setFactory(new DefaultGameFactory() {
-			@Override
-			public DiceStrategy getDiceStrategy() {
-				return new StaticDiceStrategy(6, 2);
-			}
-		}).getBattleStrategy();
+		BattleStrategy bs = GameManager.setFactory(gameFactory).getBattleStrategy();
+
+		when(gameFactory.getDiceStrategy()).thenReturn(new StaticDiceStrategy(6, 2));
 
 		TestBattleListener tbl = new TestBattleListener();
 
@@ -83,12 +101,9 @@ public class BasicUnitBattleTest {
 	@Test
 	public void testSuccesfulArcherVSHeavyCavalryAttack() {
 
-		BattleStrategy bs = GameManager.setFactory(new DefaultGameFactory() {
-			@Override
-			public DiceStrategy getDiceStrategy() {
-				return new StaticDiceStrategy(5, 4);
-			}
-		}).getBattleStrategy();
+		BattleStrategy bs = GameManager.setFactory(gameFactory).getBattleStrategy();
+
+		when(gameFactory.getDiceStrategy()).thenReturn(new StaticDiceStrategy(5, 4));
 
 		TestBattleListener tbl = new TestBattleListener();
 
@@ -104,12 +119,9 @@ public class BasicUnitBattleTest {
 	@Test
 	public void testNonSuccesfulArcherVSHeavyCavalryAttack() {
 
-		BattleStrategy bs = GameManager.setFactory(new DefaultGameFactory() {
-			@Override
-			public DiceStrategy getDiceStrategy() {
-				return new StaticDiceStrategy(4);
-			}
-		}).getBattleStrategy();
+		BattleStrategy bs = GameManager.setFactory(gameFactory).getBattleStrategy();
+
+		when(gameFactory.getDiceStrategy()).thenReturn(new StaticDiceStrategy(4));
 
 		TestBattleListener tbl = new TestBattleListener();
 
@@ -125,12 +137,9 @@ public class BasicUnitBattleTest {
 	@Test
 	public void testDefendedArcherVSHeavyCavalryAttack() {
 
-		BattleStrategy bs = GameManager.setFactory(new DefaultGameFactory() {
-			@Override
-			public DiceStrategy getDiceStrategy() {
-				return new StaticDiceStrategy(6, 3);
-			}
-		}).getBattleStrategy();
+		BattleStrategy bs = GameManager.setFactory(gameFactory).getBattleStrategy();
+
+		when(gameFactory.getDiceStrategy()).thenReturn(new StaticDiceStrategy(6, 3));
 
 		TestBattleListener tbl = new TestBattleListener();
 
@@ -146,12 +155,9 @@ public class BasicUnitBattleTest {
 	@Test
 	public void testSuccesfulPikemanVSHeavyCavalryAttack() {
 
-		BattleStrategy bs = GameManager.setFactory(new DefaultGameFactory() {
-			@Override
-			public DiceStrategy getDiceStrategy() {
-				return new StaticDiceStrategy(4, 4);
-			}
-		}).getBattleStrategy();
+		BattleStrategy bs = GameManager.setFactory(gameFactory).getBattleStrategy();
+
+		when(gameFactory.getDiceStrategy()).thenReturn(new StaticDiceStrategy(4, 4));
 
 		TestBattleListener tbl = new TestBattleListener();
 
@@ -167,12 +173,9 @@ public class BasicUnitBattleTest {
 	@Test
 	public void testNonSuccesfulPikemandVSHeavyCavalryAttack() {
 
-		BattleStrategy bs = GameManager.setFactory(new DefaultGameFactory() {
-			@Override
-			public DiceStrategy getDiceStrategy() {
-				return new StaticDiceStrategy(3);
-			}
-		}).getBattleStrategy();
+		BattleStrategy bs = GameManager.setFactory(gameFactory).getBattleStrategy();
+
+		when(gameFactory.getDiceStrategy()).thenReturn(new StaticDiceStrategy(3));
 
 		TestBattleListener tbl = new TestBattleListener();
 
@@ -188,12 +191,9 @@ public class BasicUnitBattleTest {
 	@Test
 	public void testDefededPikemandVSHeavyCavalryAttack() {
 
-		BattleStrategy bs = GameManager.setFactory(new DefaultGameFactory() {
-			@Override
-			public DiceStrategy getDiceStrategy() {
-				return new StaticDiceStrategy(4, 3);
-			}
-		}).getBattleStrategy();
+		BattleStrategy bs = GameManager.setFactory(gameFactory).getBattleStrategy();
+
+		when(gameFactory.getDiceStrategy()).thenReturn(new StaticDiceStrategy(4, 3));
 
 		TestBattleListener tbl = new TestBattleListener();
 
