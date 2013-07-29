46803c8e013be4f8e8ed654281faa025986f223f
ymer <jonatan.smp@gmail.com>
2013-07-29 15:10:07 +0200

    Use AI advancer for now, because it doesn't think forward (which could complicate things.)

--- python/ai_advancer.py
+++ python/ai_advancer.py
@@ -8,14 +8,14 @@ import settings
 def get_action(actions, g):
     
     for action in actions:
-        action.score = action.end_position[1]
+        action.score = action.end_position.row
         
-        if action.end_position[1] > action.start_position[1]:
+        if action.end_position.row > action.start_position.row:
             action.score += 1
         
         if action.is_attack():
             action.score += 0.25
-            if action.attack_position[1] > action.end_position[1] and action.is_move_with_attack():
+            if action.attack_position.row > action.end_position.row and action.is_move_with_attack():
                 action.score += 0.5
     
     rnd.shuffle(actions)
@@ -24,19 +24,3 @@ def get_action(actions, g):
     if settings.document_ai_actions:
         m.document_actions(actions, g)
     return actions[0]
-    
-
-def put_counter(g):
-    def decide_counter(unit):
-        unit.defence_counters += 1
-
-    for unit in g.units[0].values():
-        if unit.xp == 2:
-            if unit.defence + unit.defence_counters == 4:
-                unit.attack_counters += 1
-            else:
-                if not unit.attack:
-                    unit.defence_counters += 1
-                else:
-                    decide_counter(unit)
-            unit.xp = 0
