32ace3ae03786f0216aa5b67993b5a6225d24c9f
ymer <jonatan.smp@gmail.com>
2013-07-29 15:08:55 +0200

    Clean up this mess

--- python/action.py
+++ python/action.py
@@ -67,131 +67,8 @@ class Action(object):
 
         return action
 
-    @classmethod
-    def from_document_simple(cls, document):
-        document_copy = copy(document)
-
-        simple_attributes = {"start_position", "end_position", "attack_position", "ability_position",
-                             "move_with_attack", "ability", "sub_actions"}
-        convert_attributes = {"start_position", "end_position", "attack_position", "ability_position"}
-
-        read_attributes = set(attribute for attribute in simple_attributes if attribute in document and document[attribute])
-
-        for attribute in convert_attributes & read_attributes:
-            document_copy[attribute] = Position.from_string(document_copy[attribute])
-
-        if "sub_actions" in document_copy and document_copy["sub_actions"]:
-            document_copy["sub_actions"] = [cls.from_document_simple(sub_action_document)
-                                            for sub_action_document in document_copy["sub_actions"]]
-
-        return cls(**document_copy)
-
-    def attribute_representation(self):
-        return str(self.__dict__)
-
-    def get_simple_string(self):
-        if self.unit:
-            representation = self.unit.name
-        elif self.unit_reference:
-            representation = self.unit_reference.name
-        else:
-            representation = "Unit"
-
-        if self.target_reference:
-            target = self.target_reference.name
-        else:
-            target = "Unit"
-
-        if self.start_position != self.end_position:
-            representation += " move from " + common.position_to_string(self.start_position)
-            representation += " to " + common.position_to_string(self.end_position)
-            if self.is_attack():
-                representation += " and"
-        else:
-            representation += " at " + common.position_to_string(self.start_position)
-
-        if self.is_attack() and not self.is_move_with_attack():
-            representation += " attack " + target + " " + common.position_to_string(self.attack_position)
-
-        if self.is_attack() and self.is_move_with_attack():
-            representation += " attack-move " + target + " " + common.position_to_string(self.attack_position)
-
-        if self.is_ability():
-            representation += " use " \
-                              + self.ability\
-                              + " on " \
-                              + target \
-                              + " " \
-                              + common.position_to_string(self.ability_position)
-
-        return representation
-
-    def get_basic_string(self):
-        representation = self.unit_reference.name
-
-        if self.start_position != self.end_position:
-            representation += " move from " + common.position_to_string(self.start_position)
-            representation += " to " + common.position_to_string(self.end_position)
-            if self.is_attack():
-                representation += " and"
-        else:
-            representation += " at " + common.position_to_string(self.start_position)
-
-        if self.is_attack() and self.move_with_attack != MoveOrStay.MOVE:
-            representation += " attack " + self.target_reference.name + " " + common.position_to_string(self.attack_position)
-
-        if self.is_attack() and self.move_with_attack == MoveOrStay.MOVE:
-            representation += " attack-move " + self.target_reference.name + " " + common.position_to_string(self.attack_position)
-
-        if self.is_ability():
-            representation += " use "\
-                              + self.ability\
-                              + " on "\
-                              + self.target_reference.name\
-                              + " "\
-                              + common.position_to_string(self.ability_position)
-
-        return representation
-
-    def get_battle_outcome_string(self):
-        if self.outcome:
-            return self.outcome
-        else:
-            return ""
-
-    def get_full_battle_outcome_string(self):
-        representation = ""
-        if self.rolls:
-            attack = battle.get_attack_rating(self.unit_reference, self.target_reference, self)
-            defence = battle.get_defence_rating(self.unit_reference, self.target_reference, attack)
-
-            representation += "Stats A: " + str(attack) + ", D: " + str(defence)
-            representation += " Rolls A: " + str(self.rolls[0]) + " D: " + str(self.rolls[1])
-            representation += ", " + self.outcome
-
-        for sub_action in self.sub_actions:
-            representation += "\n"
-            representation += "and attack " + common.position_to_string(sub_action.attack_position)
-            if sub_action.rolls:
-                attack = battle.get_attack_rating(self.unit_reference, self.target_reference, self)
-                defence = battle.get_defence_rating(self.unit_reference, self.target_reference, attack)
-
-                representation += ", Stats A: " + str(attack) + ", D: " + str(defence)
-                representation += " Rolls A: " + str(self.rolls[0]) + " D: " + str(self.rolls[1])
-                representation += ", " + self.outcome
-
-        return representation
-
     def __repr__(self):
-        return self.get_simple_string()
-
-    def full_string(self):
-        return self.get_basic_string() + ", "\
-                                       + self.get_battle_outcome_string() + "\n" \
-                                       + self.get_full_battle_outcome_string()
-
-    def string_with_outcome(self):
-        return self.get_basic_string() + ", " + self.get_battle_outcome_string()
+        return document_to_string(self.to_document())
 
     def __eq__(self, other):
         basic_attributes = ["start_position",
@@ -210,29 +87,14 @@ class Action(object):
         sub_action_docs = [sub_action.to_document() for sub_action in self.sub_actions]
 
         return {"action_number": action_number,
-                "start_position": common.position_to_string(self.start_position),
-                "end_position": common.position_to_string(self.end_position),
-                "attack_position": common.position_to_string(self.attack_position),
-                "ability_position": common.position_to_string(self.ability_position),
+                "start_position": str(self.start_position),
+                "end_position": str(self.end_position),
+                "attack_position": str(self.attack_position),
+                "ability_position": str(self.ability_position),
                 "move_with_attack": self.move_with_attack,
                 "ability": self.ability,
                 "sub_actions": sub_action_docs,
                 "created_at": self.created_at}
-
-    def to_document_simple(self):
-        sub_action_docs = [sub_action.to_document_simple() for sub_action in self.sub_actions]
-        simple_attributes = {"start_position", "end_position", "attack_position", "ability_position",
-                             "move_with_attack", "ability", "sub_actions"}
-        convert_attributes = {"start_position", "end_position", "attack_position", "ability_position"}
-        write_attributes = set(attribute for attribute in simple_attributes if getattr(self, attribute))
-        document = {}
-        for attribute in write_attributes & convert_attributes:
-            document[attribute] = common.position_to_string(getattr(self, attribute))
-        for attribute in write_attributes - convert_attributes:
-            document[attribute] = getattr(self, attribute)
-        if sub_action_docs:
-            document["sub_actions"] = sub_action_docs
-        return document
 
     def is_move_with_attack(self):
         return self.move_with_attack == MoveOrStay.MOVE
@@ -257,13 +119,13 @@ class Action(object):
         return bool(self.ability)
 
     def is_lancing(self):
-        return self.unit_reference.has("lancing") and self.is_attack() and self.distance_to_target() >= 3
+        return self.unit.has("lancing") and self.is_attack() and self.distance_to_target() >= 3
 
     def is_lancing_II(self):
-        return self.unit_reference.has("lancing_II") and self.is_attack() and self.distance_to_target() >= 4
+        return self.unit.has("lancing_II") and self.is_attack() and self.distance_to_target() >= 4
 
     def is_push(self):
-        return self.unit_reference.has("push") and self.is_attack()
+        return self.unit.has("push") and self.is_attack()
 
     def is_crusading(self, gamestate):
         return any(unit for unit in self.surrounding_friendly_units(gamestate) if unit.has("crusading"))
