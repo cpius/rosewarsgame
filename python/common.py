4ee2e7bd9e4194580a259cf1f65661c0f4570ba8
ymer <jonatan.smp@gmail.com>
2013-07-31 11:10:27 +0200

    move memoized to common

--- python/common.py
+++ python/common.py
@@ -1,6 +1,7 @@
 from json import JSONEncoder, dumps
 from datetime import datetime
 from bson import ObjectId
+import collections
 
 
 class Direction:
@@ -276,3 +277,29 @@ class CustomJsonEncoder(JSONEncoder):
 
 def document_to_string(document):
     return dumps(document, indent=4, cls=CustomJsonEncoder)
+
+
+class memoized(object):
+    """Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    """
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+
+    def __repr__(self):
+        return self.func.__doc__
+
+    def __get__(self, obj):
+        return functools.partial(self.__call__, obj)
