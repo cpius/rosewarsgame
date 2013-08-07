617c0c1eddeef58122fdf81dede088dd8aee408c
ymer <jonatan.smp@gmail.com>
2013-08-07 18:51:17 +0200

    Implement control with keyboard arrow buttons. Also "Unit" type scenario.

--- python/tutorial.py
+++ python/tutorial.py
@@ -7,9 +7,49 @@ from gamestate import Gamestate
 from game import Game
 from player import Player
 import os
+import units as units_module
 
 shading_blue = pygame.Color(0, 0, 100, 160)
 shading_red = pygame.Color(100, 0, 0, 160)
+view = view_module.View()
+
+
+def draw_gamestate(path):
+    gamestate = Gamestate.from_file(path + "Gamestate.json")
+    players = [Player("Green", "Human"), Player("Red", "Human")]
+    game = Game(players, gamestate)
+    view.draw_tutorial(game)
+
+
+def draw_marked(path):
+    marked_blue = [Position.from_string(position) for position in
+                   json.loads(open(path + "Marked_blue.marked").read())["tiles"]]
+    view.shade_positions(marked_blue, shading_blue)
+
+    if os.path.exists(path + "Marked_red.marked"):
+        marked_red = [Position.from_string(position) for position in
+                      json.loads(open(path + "Marked_red.marked").read())["tiles"]]
+        view.shade_positions(marked_red, shading_red)
+
+
+def draw_description(path):
+    description = open(path + "Description.txt").readlines()
+    view.draw_tutorial_message(description)
+
+
+def draw_empty_gamestate():
+    gamestate = Gamestate({}, {}, 2)
+    players = [Player("Green", "Human"), Player("Red", "Human")]
+    game = Game(players, gamestate)
+    view.draw_tutorial(game)
+
+
+def draw_unit(path):
+    unit_name = open(path + "Unit.txt").readline()
+    unit = getattr(units_module, unit_name.replace(" ", "_"))()
+    view.show_unit_zoomed(unit)
+    description = open(path + "Description.txt").readlines()
+    view.draw_tutorial_message(description, 550)
 
 
 def run_tutorial():
@@ -17,44 +57,36 @@ def run_tutorial():
         with open("./tutorial/list.txt") as file:
             scenarios = [line.split(": ") for line in file]
     else:
-        scenarios = [["Moves_3", "Move"]]
+        scenarios = [["Unit_1", "Unit"]]
 
     exit = False
     index = 0
+
     while index < len(scenarios) and not exit:
         if scenarios[index][1].strip() == "Move":
             path = "./tutorial/" + scenarios[index][0] + "/"
 
-            gamestate = Gamestate.from_file(path + "Gamestate.json")
-            players = [Player("Green", "Human"), Player("Red", "Human")]
-            game = Game(players, gamestate)
-            view = view_module.View()
-            view.draw_tutorial(game)
+            draw_gamestate(path)
+            draw_marked(path)
+            draw_description(path)
 
-            marked_blue = [Position.from_string(position) for position in
-                           json.loads(open(path + "Marked_blue.marked").read())["tiles"]]
-            view.shade_positions(marked_blue, shading_blue)
+        elif scenarios[index][1].strip() == "Unit":
+            path = "./tutorial/" + scenarios[index][0] + "/"
 
-            if os.path.exists(path + "Marked_red.marked"):
-                marked_red = [Position.from_string(position) for position in
-                              json.loads(open(path + "Marked_red.marked").read())["tiles"]]
-                view.shade_positions(marked_red, shading_red)
+            draw_empty_gamestate()
+            draw_unit(path)
 
-            description = open(path + "Description.txt").readlines()
-            view.draw_tutorial_message(description)
+        while True:
+            event = pygame.event.wait()
 
-            while True:
-                event = pygame.event.wait()
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        break
-                    elif event.button == 3:
-                        index -= 2
-                        break
-                elif quit_game_requested(event):
-                    exit = True
-                    break
+            if move_forward_requested(event):
+                break
+            elif move_backward_requested(event):
+                index -= 2
+                break
+            elif quit_game_requested(event):
+                exit = True
+                break
 
         index += 1
 
@@ -66,6 +98,13 @@ def quit_game_requested(event):
 def command_q_down(key):
     return key == K_q and (pygame.key.get_mods() & KMOD_LMETA or pygame.key.get_mods() & KMOD_RMETA)
 
+
+def move_forward_requested(event):
+    return (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1) or (event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT)
+
+
+def move_backward_requested(event):
+    return (event.type == pygame.MOUSEBUTTONDOWN and event.button == 3) or (event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT)
 
 if __name__ == "__main__":
     use_list = True
