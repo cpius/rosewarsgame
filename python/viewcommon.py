fb804f203cecdcbb389da20af3641e2f667e5007
ymer <jonatan.smp@gmail.com>
2013-08-07 17:12:20 +0200

    remove print

--- python/viewcommon.py
+++ python/viewcommon.py
@@ -64,7 +64,6 @@ def split_lines(lines, line_length):
     new_lines = []
     for line in lines:
         if line:
-            print "split"
             split_lines = textwrap.wrap(line, line_length)
         else:
             split_lines = [""]
