3528a392c2a3c425dd656a3e25eda3eee601dd76
Mads Dorup <mads@dorup.dk>
2013-08-18 18:54:55 +0200

    Rename attrs => attributes

--- python/action.py
+++ python/action.py
@@ -117,8 +117,8 @@ class Action(object):
         return document
 
     def to_document(self):
-        attrs = ["number", "start_at", "end_at", "target_at", "created_at"]
-        document = dict((attr, str(getattr(self, attr))) for attr in attrs if getattr(self, attr))
+        attributes = ["start_at", "end_at", "target_at", "created_at"]
+        document = dict((attr, str(getattr(self, attr))) for attr in attributes if getattr(self, attr))
         if self.ability:
             document["ability"] = Ability.name[self.ability]
         if not self.move_with_attack is None:
