b95df9f445985f913e9c95d3077f6a59a7f56eab
ymer <jonatan.smp@gmail.com>
2013-08-22 10:07:48 +0200

    Remove erronous parenthesis

--- python/common.py
+++ python/common.py
@@ -191,7 +191,7 @@ effect_descriptions = {
 ability_descriptions = {
     "bribe": {
         1: "You can use an opponent's unit this turn. Your opponent can't use it on his next turn. You can't bribe "
-        "the same unit on your next turn. The unit gets +1A until end of turn."},
+        "the same unit on your next turn. The unit gets +1A until end of turn.",
         2: "You can use an opponent's unit this turn. Your opponent can't use it on his next turn. You can't bribe "
         "the same unit on your next turn. The unit gets +2A until end of turn."},
     "improve_weapons": {
