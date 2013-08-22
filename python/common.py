3764e8c2ec8f757426d9511c902ad426069e37e9
ymer <jonatan.smp@gmail.com>
2013-08-22 21:13:10 +0200

    Oops. Deep error to find.

--- python/common.py
+++ python/common.py
@@ -12,7 +12,7 @@ class Direction:
     """
 
     to_coordinates = {"Left": (-1, 0), "Right": (1, 0), "Down": (0, -1), "Up": (0, 1),
-                      "Up_Left": (-1, 1), "Up_Right": (1, 1), "Down_Left": (-1, -1), "Down_Right": (-1, 1)}
+                      "Up_Left": (1, -1), "Up_Right": (1, 1), "Down_Left": (-1, -1), "Down_Right": (-1, 1)}
 
     def __init__(self, name):
         self.x, self.y = self.to_coordinates[name]
