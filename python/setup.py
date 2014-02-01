9821cd70d6d201da03907234e56eea8a6769f291
Jonatan Pallesen <jonatan.smp@gmail.com>
2014-02-01 19:15:54 -0200

    Some cosmetic changes. Also, the units_name -> unit transformation is not made until the end, in order to potentially save time if there are retrys.

--- python/setup.py
+++ python/setup.py
@@ -4,8 +4,6 @@ from common import *
 from units import Unit
 from collections import namedtuple
 
-unit_bag_size = 3
-
 required_special_units = []
 allowed_special_units = ["Berserker", "Cannon", "Crusader", "Flag Bearer", "Longswordsman", "Saboteur", "Royal Guard",
                          "Scout", "War Elephant", "Weaponsmith", "Viking", "Diplomat", "Halberdier", "Dragoon",
@@ -15,6 +13,7 @@ allowed_basic_units = ["Archer", "Ballista", "Catapult", "Knight", "Light Cavalr
 requirements = ["at_least_two_column_blocks", "at_most_one_pikeman_per_column", "at_least_one_siege_weapon",
                 "at_most_two_siege_weapons"]
 
+siege_weapons = ["Cannon", "Ballista", "Catapult"]
 
 Info = namedtuple("Info", ["allowed_rows", "copies", "protection_required"])
 
@@ -46,10 +45,13 @@ units_info = {"Archer": Info({2, 3}, 3, False),
               "Crossbow Archer": Info({2, 3}, 3, False),
               "Fire Archer": Info({2, 3}, 3, False)}
 
+board_rows = [1, 2, 3, 4]
+board_columns = [1, 2, 3, 4, 5]
+
 
 class Tiles_bag(object):
     def __init__(self):
-        self.tiles = [Position(column, row) for column in board_columns for row in board_rows]
+        self.tiles = board.copy()
         
     def pick_from_row(self, rows):
         pick = random.choice([tile for tile in self.tiles if tile.row in rows])
@@ -57,9 +59,8 @@ class Tiles_bag(object):
         return pick
 
     def pick_protected_tile(self, rows):
-        possible_tiles = [Position(column, row) for column in board_columns for
-                          row in [2, 3] if Position(column, row) in self.tiles and
-                          Position(column, row + 1) not in self.tiles]
+        possible_tiles = [Position(column, row) for column in board_columns for row in [2, 3]
+                          if Position(column, row) in self.tiles and Position(column, row + 1) not in self.tiles]
 
         pick = random.choice([tile for tile in possible_tiles if tile.row in rows])
         self.tiles.remove(pick)
@@ -78,65 +79,47 @@ class Unit_bag(object):
     def has_units(self):
         return self.units
 
-    def remove_units(self, name_list):
-        self.units = [unit for unit in self.units if unit not in name_list]
-
-    def remove_one_unit(self, name):
-        self.units.remove(name)
-
-
-board_rows = [1, 2, 3, 4]
-board_columns = [1, 2, 3, 4, 5]
-
-siege_weapons = ["Ballista", "Catapult", "Cannon"]
-
 
 def at_least_two_column_blocks(units):
     """ Tests whether there on each column are at least two 'blocks'.
     A block is either a unit, or a Pikeman zoc tile. """
     
-    columns = [position.column + x for x in [-1, +1] for position, unit in units.items() if unit.name == "Pikeman"] + \
-              [position.column for position in units]
+    blocks = [pos.column + n for n in [-1, 0, +1] for pos, unit in units.items() if unit == "Pikeman"] + \
+             [pos.column for pos in units]
 
-    return not any(columns.count(column) < 2 for column in board_columns)
+    return all(blocks.count(column) >= 2 for column in board_columns)
      
 
 def at_most_one_pikeman_per_column(units):
-    columns = [position.column for position, unit in units.items() if unit.name == "Pikeman"]
-    
-    return not any(columns.count(column) > 1 for column in board_columns)
+    return not any(column for column in board_columns if sum(1 for pos, unit in units.items() if
+                                                             pos.column == column and unit == "Pikeman") > 1)
 
 
 def at_least_one_siege_weapon(units):
-    return any(unit.name in siege_weapons for unit in units.values())
+    return any(unit in siege_weapons for unit in units.values())
 
 
 def at_most_two_siege_weapons(units):
-    return sum(1 for unit in units.values() if unit.name in siege_weapons) <= 2
+    return sum(1 for unit in units.values() if unit in siege_weapons) <= 2
 
 
 def get_units():
 
-    beginner_mode = get_setting("Beginner_mode")
-    if beginner_mode:
-        basic_unit_count = 9
-        special_unit_count = 0
+    if get_setting("Beginner_mode"):
+        basic_unit_count, special_unit_count = 9, 0
     else:
-        special_unit_count = 3
-        basic_unit_count = 6
+        basic_unit_count, special_unit_count = 3, 6
     
     def select_basic_units(basic_units_bag):
-        return [Unit.make(basic_units_bag.pick()) for _ in range(basic_unit_count)]
+        return [basic_units_bag.pick() for _ in range(basic_unit_count)]
 
     def select_special_units(special_units_first_bag, special_units_second_bag):
-
         special_units = []
         while len(special_units) < special_unit_count and special_units_first_bag.has_units():
-            unit = Unit.make(special_units_first_bag.pick())
-            special_units.append(unit)
+            special_units.append(special_units_first_bag.pick())
 
         while len(special_units) < special_unit_count:
-            special_units.append(Unit.make(special_units_second_bag.pick()))
+            special_units.append(special_units_second_bag.pick())
 
         return special_units
 
@@ -152,16 +135,15 @@ def get_units():
     def place_units_on_board(units_list, tiles_bag):
 
         units = {}
-        unprotected_units = [unit for unit in units_list if not units_info[unit.name].protection_required]
-        protected_units = [unit for unit in units_list if units_info[unit.name].protection_required]
+        unprotected_units = [unit for unit in units_list if not units_info[unit].protection_required]
+        protected_units = [unit for unit in units_list if units_info[unit].protection_required]
 
         for unit in unprotected_units:
-            allowed_rows = units_info[unit.name].allowed_rows.copy()
-            position = tiles_bag.pick_from_row(allowed_rows)
+            position = tiles_bag.pick_from_row(units_info[unit].allowed_rows)
             units[position] = unit
 
         for unit in protected_units:
-            position = tiles_bag.pick_protected_tile(units_info[unit.name].allowed_rows)
+            position = tiles_bag.pick_protected_tile(units_info[unit].allowed_rows)
             units[position] = unit
 
         return units
@@ -182,7 +164,10 @@ def get_units():
 
         if any(not globals()[requirement](units) for requirement in requirements):
             continue
-             
+
+        for position, unit in units.items():
+            units[position] = Unit.make(unit)
+
         return units
 
 
