f804540d8c215625a7321c3767724b7be7fd15e7
Jonatan Pallesen <jonatan.smp@gmail.com>
2014-03-26 15:16:35 +0100

    Add remove function to server

--- python/server.py
+++ python/server.py
@@ -316,6 +316,12 @@ def register_upgrade(action_document, gamestate, game_id):
         return {"Status": "Error", "Message": message}
 
 
+@app.get("games/remove/<game_id>")
+def remove(game_id):
+    games = get_collection("games")
+    games.remove({"_id": ObjectId(game_id)})
+    
+
 def register_move_with_attack(action_document, game_id, gamestate):
     action_collection = get_collection("actions")
 
