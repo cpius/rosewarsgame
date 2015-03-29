6c9653674d1f0e0ad462c4eade1b218ea92371dd
ymer <jonatan.smp@gmail.com>
2015-03-29 19:02:14 +0200

    On second thought, flyt AI tests til /tests

--- Python/run_tests.py
+++ Python/run_tests.py
@@ -1,15 +1,16 @@
-from tests import utester, setup_tester, controller_tester, speed_tester2
+from tests import utester, setup_tester, controller_tester, speed_tester2, ai_tester
 
 
 testdict = {
     1:  ("Utester", utester.run),
     2:  ("Setup tester", setup_tester.run),
     3: ("Controller tester", controller_tester.run),
-    4: ("Speed Tester", speed_tester2.run)
+    4: ("Speed tester", speed_tester2.run),
+    5: ("AI tester", ai_tester.run)
 }
 
 
-usetests = [3]
+usetests = [5]
 
 
 if __name__ == '__main__':
