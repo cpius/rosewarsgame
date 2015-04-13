b21cf3b6bb0efc931fe34d1b2be9832d86847fa5
ymer <jonatan.smp@gmail.com>
2015-04-13 21:37:22 +0200

    remove extra []

--- python/game/player.py
+++ python/game/player.py
@@ -19,7 +19,7 @@ class Player(object):
 
     @classmethod
     def from_document(cls, document):
-        return cls(document["color"], Intelligence([document["intelligence"]]), document["profile"])
+        return cls(document["color"], Intelligence(document["intelligence"]), document["profile"])
 
     def to_document(self):
         return {
